b6187e7892c262b2d1710e2048e5b679e809bf18
hector-teke <hectorguzman422@uma.es>
2023-12-15 01:52:56 +0100

    Initial Commit

---
+++ RSAcipher.py
@@ -0,0 +1,111 @@
+import math
+import random
+from sympy import isprime
+
+###################### OT -> Integer
+
+def text_to_vector(OT):
+    vector = []
+    while len(OT) >= 5:
+        vector.append(block_to_int(OT[-5:]))
+        # print(OT[-5:], " \tnum: ", vector)
+        OT = OT[0:-5]
+
+    if len(OT) > 0:
+        vector.append(block_to_int(OT))
+        # print(OT, "  \tnum: ", vector)
+
+    return vector[::-1]
+
+
+def block_to_int(blk):
+    bin_string = ""
+
+    for c in blk:
+        bin_string += int_to_bin(ord(c))
+
+    return int(bin_string, 2)
+
+
+# 8-bit codification of a given integer
+def int_to_bin(n):
+    bin_string = bin(n)[2:]
+    return (8 - len(bin_string)) * '0' + bin_string
+
+
+###################### Integer -> OT
+
+def vector_to_text(vector):
+    OT = ""
+    for n in vector:
+        OT += int_to_block(n)
+
+    return OT
+
+
+def int_to_block(num):
+    blk = ""
+    bin_string = bin(num)[2:]
+    bin_string = (8 - (len(bin_string) % 8)) * '0' + bin_string
+
+    for i in range(0, len(bin_string), 8):
+        blk += chr(int(bin_string[i:i + 8], 2))
+        # print(i, "\t", bin_string[i:i+8])
+
+    return blk
+
+
+###################### Encryption and decryption
+
+def encryption(OT, e, n):
+    CT = []
+    for blk in text_to_vector(OT):
+        CT.append(pow(blk, e, n))
+
+    return CT
+
+
+def decryption(CT, d, n):
+    OT = []
+    for blk in CT:
+        OT.append(pow(blk, d, n))
+
+    return vector_to_text(OT)
+
+
+###################### Keys generation
+
+def generate_primes(bits):
+    p1 = None
+    p2 = None
+
+    while p1 == p2:
+        p1 = random.getrandbits(bits)
+        p1 |= (1 << bits - 1) | 1
+        while not isprime(p1):
+            p1 += 2  # Avoid even numbers
+
+        p2 = random.getrandbits(bits)
+        p2 |= (1 << bits - 1) | 1
+        while not isprime(p2):
+            p2 += 2  # Avoid even numbers
+
+    return p1, p2
+
+
+def select_e(phi):
+    while True:
+        e = random.randint(2, phi - 1)      # 1 < e < phi
+        if math.gcd(e, phi) == 1:  # GCD(e, phi) = 1
+            break
+    return e
+
+
+def generate_keys(bits):
+    p, q = generate_primes(bits)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = select_e(phi)
+    d = pow(e, -1, phi)
+
+    return e, d, n      # Pub, Priv, Module
---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+
+from RSAcipher import encryption, decryption, generate_keys
+import tkinter as tk
+from tkinter import filedialog
+import base64
+import sys
+
+def select_file(extension):
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename(
+        title="Select file",
+        filetypes=[("Public key file", "*." + extension)]
+    )
+
+    return file_path
+
+def read_key_file(path):            # * for any file
+    with open(path, 'r') as file:
+        content_base64 = file.read()
+
+    content = base64.b64decode(content_base64).decode('utf-8')
+
+    module, key = content[1:-1].split(',')
+
+    return module, key
+
+def export_keys(modulo, pubkey, privkey):
+    content = f'({modulo},{pubkey})'
+    content_bytes = content.encode('utf-8')
+    content_base64 = base64.b64encode(content_bytes).decode('utf-8')
+
+    root = tk.Tk()
+    root.withdraw()
+
+    file1_path = filedialog.asksaveasfilename(
+        defaultextension=".pub",
+        filetypes=[("Public key file", "*.pub")],
+    )
+
+    with open(file1_path, 'w') as file:
+        file.write(content_base64)
+    print(f"File saved at {file1_path}")
+
+    file2_path = filedialog.asksaveasfilename(
+        defaultextension=".pub",
+        filetypes=[("Private key file", "*.priv")],
+    )
+
+    with open(file2_path, 'w') as file:
+        file.write(content_base64)
+    print(f"File saved at {file2_path}")
+
+
+def file_description(path):
+    info = os.stat(path)
+    print(f'Name: {os.path.basename(path)}\nPath: {path}\nSize: {info.st_size} bytes')
+
+
+def file_hash(path):  # compute hash of a given file
+
+
+if __name__ == '__main__':
+
+    pub, priv, mod = generate_keys(112)
+
+    print(f'Pubkey: {pub}, Module: {mod}')
+    print(f'Privkey: {priv}, Module: {mod}')
+
+    export_keys(mod, pub, priv)
+
+    file = select_file("pub")
+
+    file_description(file)
+
+    module, pubkey = read_key_file(file)
+
+    print(f'Pubkey: {pubkey}, Module: {module}')
+
+
+
+
+
+
+
+
+
